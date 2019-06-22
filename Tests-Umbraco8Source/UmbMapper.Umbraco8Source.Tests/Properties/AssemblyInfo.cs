75184decc13b33a6159772885cc3b21696776394
Euan Rae <euan37@hotmail.com>
2019-06-22 12:40:35 +0100

    WIP - trying to inherit from Umbraco unit test

---
+++ Tests-Umbraco8Source/UmbMapper.Umbraco8Source.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UmbMapper.Umbraco8Source.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UmbMapper.Umbraco8Source.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e09adcc0-4875-477c-a9b7-c3b13b3adcf1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests-Umbraco8Source/UmbMapper.Umbraco8Source.Tests/TestUmbUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Xml;
+using Moq;
+using NUnit.Framework;
+using Umbraco.Core;
+using Umbraco.Core.Cache;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Configuration;
+using Umbraco.Core.Configuration.UmbracoSettings;
+using Umbraco.Core.Services;
+using Umbraco.Tests.Cache.PublishedCache;
+using Umbraco.Tests.LegacyXmlPublishedCache;
+using Umbraco.Tests.TestHelpers;
+using Umbraco.Tests.Testing;
+using Umbraco.Tests.Testing.Objects.Accessors;
+using Umbraco.Web;
+using Umbraco.Web.PublishedCache;
+using Umbraco.Web.Routing;
+using Umbraco.Web.Security;
+
+namespace UmbMapper.Umbraco8Source.Tests
+{
+    [TestFixture]
+    [UmbracoTest(Database = UmbracoTestOptions.Database.NewSchemaPerFixture)]
+    public class UmbMapper8TestTest : UmbracoTestBase
+    {
+        
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            
+        }
+
+        [Test]
+        public void TestRuns()
+        {
+            Assert.IsTrue(true);
+        }
+
+
+        
+    }
+}
